01a7a8800eef5d08a6706a8384b87a922d321e52
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-07 14:00:07 +0200

    Ajout .gitignore, test unitaires pour Film

---
+++ tests/film.spec.ts
@@ -0,0 +1,32 @@
+import { Film } from "../classes/Film"
+import { Trailer } from "../classes/Trailer";
+import * as assert from "chai"
+
+let genre: string[] = ["Action and Adventure"]
+let actors: string[] = [
+    "Dean Ambrose",
+    "Roger Cross",
+    "Daniel Cudmore",
+    "Lochlyn Munro",
+    "Ty Olsson",
+    "Sarah Smyth"
+]
+let trailers: Array<Trailer> = [new Trailer("Mon, 27 Jul 2015 00:00:00 -0700", "/trailers/lions_gate/12rounds3lockdown/", "Trailer", false, true)];
+let film = new Film("12 Rounds 3: Lockdown", "Fri, 11 Sep 2015 00:00:00 -0700", "Lionsgate", "http://trailers.apple.com/trailers/lions_gate/12rounds3lockdown/images/poster.jpg", "/trailers/lions_gate/12rounds3lockdown/", "R", genre, "Stephen Reynolds", actors, trailers);
+
+describe('Film', () => {
+    it('Should have 6 actors', () => {
+
+        assert.expect(film.actors.length).to.equal(6);
+    });
+    
+    it('Should have 1 genre', () => {
+
+        assert.expect(film.genre.length).to.equal(1);
+    });
+
+    it('Should have 1 trailer', () => {
+
+        assert.expect(film.trailers.length).to.equal(1);
+    });
+});
