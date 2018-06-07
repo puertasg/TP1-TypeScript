79180400aea38d8ab7a41cabd1b408cefabf5854
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-07 14:06:20 +0200

    Ajout tests unitaires pour Boisson

---
+++ tests/boisson.spec.ts
@@ -0,0 +1,18 @@
+import { Boisson } from "../classes/Boisson";
+import * as assert from "chai"
+
+let boisson = new Boisson("Verre d'eau", false, true);
+
+describe("Boisson", () => {
+    it("Alchool flag should be false", () => {
+        assert.expect(boisson.alcholisee).to.be.false;
+    });
+
+    it("Bio flag should be true", () => {
+        assert.expect(boisson.bio).to.be.true;
+    });
+
+    it("Name should be Verre d'eau", () => {
+        assert.expect(boisson.nom).to.equal("Verre d'eau");
+    });
+})
